72306d14248234b97a91e370495c37665f6a6557
Satish Kumar Anbalagan <anbalagan.s@northeastern.edu>
2020-10-05 02:55:17 -0400

    changes in write & print function design

--- part-1.c
+++ part-1.c
@@ -61,47 +61,44 @@ void exit(int err);
  * Function to read a complete line from stdin.
  * 
  * @param pInput: pointer to the buffer in char
- * @param max_length: maximum length of the buffer in int
  * @return length of the characters read in integer
  */
-int readline(char *pInput, int max_length);
+int readline(char *pInput);
 
 /*
  * Function to write a complete line to stdout.
  * 
  * @param pInput: pointer to the buffer in char
+ * @return length of the characters written in integer
  */
-void print(void *pInput);
+int print(void *pInput);
 
 /* Function Definitions */
 
-int readline(char *pInput, int max_length)
+int readline(char *pInput)
 {
     int ret = FUNCTION_FAILURE;
     if (pInput != NULL) {
         // Read input character one at a time until EOF and newline character is found with NULL termination.
-        ret = read(STDIN_FILE_DESCRIPTOR_NUMBER, pInput, max_length);
+        ret = read(STDIN_FILE_DESCRIPTOR_NUMBER, pInput, MAX_BUFFER_SIZE);
     }
     return ret;
 }
 
-void print(void *pInput)
+int print(void *pInput)
 {
-    unsigned int length = 0;
-    char *ptr = (char*) pInput;
-    char c = *((char*) pInput);
-    
-    // Read input characters until EOF and newline character is found.
-    while (c != NULL) {
-        length += write(STDOUT_FILE_DESCRIPTOR_NUMBER, &c, 1);
-        c = *(++ptr);
+    int ret = FUNCTION_FAILURE;
+    if (pInput != NULL) {
+        // Read input characters until EOF and newline character is found.
+        ret = write(STDOUT_FILE_DESCRIPTOR_NUMBER, pInput, MAX_BUFFER_SIZE);
     }
+    return ret;
 }
 
 int read(int fd, void *ptr, int len)
 {
-    unsigned int ret = FUNCTION_FAILURE;
-    unsigned int readLength = 0;
+    int ret = FUNCTION_FAILURE;
+    int readLength = 0;
     char *cPtr = (char*) ptr;
     char c;
 
@@ -123,7 +120,24 @@ int read(int fd, void *ptr, int len)
 
 int write(int fd, void *ptr, int len)
 {
-    return syscall(__NR_write, fd, ptr, len);
+    int ret = FUNCTION_FAILURE;
+    int writtenLength = 0;
+    char *cPtr = (char*) ptr;
+    char c = *(char*) ptr;
+    
+    // with length of 0 returns zero and has no other effects
+    if(len > 0) {
+        // Read input characters until EOF and newline character is found.
+        while (c != NULL && writtenLength < len) {
+            syscall(__NR_write, fd, &c, 1);
+            ret = ++writtenLength;
+            c = *(++cPtr);
+        }
+    }
+    else {
+        ret = 0;
+    }
+    return ret;
 }
 
 void exit(int err)
@@ -143,8 +157,8 @@ void main(void)
     if(pInput != NULL) {    // NULL check, if in case malloc is used in future
         while(1) {
             print("> ");
-            readline(pInput, MAX_BUFFER_SIZE);
-            //read(STDIN_FILE_DESCRIPTOR_NUMBER, pInput, MAX_BUFFER_SIZE);
+            // readline(pInput);
+            read(STDIN_FILE_DESCRIPTOR_NUMBER, pInput, MAX_BUFFER_SIZE);
             int count = 0;
             int match = 1;
 
@@ -162,7 +176,8 @@ void main(void)
             }
 
             print("you typed: ");
-            print(pInput);
+            // print(pInput);
+            write(STDIN_FILE_DESCRIPTOR_NUMBER, pInput, MAX_BUFFER_SIZE);
         }
     }
     else {
